d16e49a357f4c44c1446dfa556666f6909af75d7
SATHVIK SVS <svssathvik77@gmail.com>
2023-12-28 22:36:22 +0530

    username unique validation done

--- controllers/userRegistration.js
+++ controllers/userRegistration.js
@@ -2,19 +2,19 @@ const userModel = require("../models/userModel.js");
 const bcrypt = require("bcrypt");
 const userRegistration = async(req,res)=>{
     try {
-        const salt = await bcrypt.genSalt();
-        const password = await bcrypt.hash(req.body.password,salt);
-        const newUser = new userModel({
-            username : req.body.username,
-            email : req.body.email,
-            password : password,
-            dp : req?.body?.dp
-        });
-        await newUser.save();
-        res.status(200).json({customResponse:"User has been successfully created!"});
+            const salt = await bcrypt.genSalt();
+            const password = await bcrypt.hash(req.body.password,salt);
+            const newUser = new userModel({
+                username : req.body.username,
+                email : req.body.email,
+                password : password,
+                dp : req?.body?.dp
+            });
+            await newUser.save();
+            res.status(200).json({customResponse:"User has been successfully created!"});
     } catch (error) {
         if (error.code === 11000 ) {
-            return res.status(400).json({ customResponse: "User with Email already exists", error: error });
+            return res.status(400).json({ customResponse: "User with Email/Username already exists", error: error });
         }
         else{
             res.status(500).json({ customResponse: "Error while saving user!", error:error });
