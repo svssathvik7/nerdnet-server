d891ba21655e05a2ffce42dad93cbd7e16686db8
SATHVIK SVS <svssathvik77@gmail.com>
2024-03-30 14:03:18 +0530

    notification creation done

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,12 +1,21 @@
 const userModel = require("../models/userModel");
 
 const addAdminController = async (req,res)=>{
-    const {receiver_mail} = req.body;
+    const {receiver_mail,admin} = req.body;
     try {
+        const adminMatch = await userModel.findOne({_id:admin});
+        const notification = {
+            from : adminMatch.username,
+            to : receiver_mail,
+            message : `${adminMatch.username} wants you to be admin!`
+        }
         const updatedPrevilage = await userModel.findOneAndUpdate({email:receiver_mail},
             {
                 $set : {
                     isAdmin : "pending"
+                },
+                $push : {
+                    notifications : notification
                 }
             });
         if(updatedPrevilage){
@@ -17,6 +26,7 @@ const addAdminController = async (req,res)=>{
             res.status(401).json({message:"User not found!",status:false});
         }
     } catch (error) {
+        console.log(error," at add admin")
         res.status(500).json({message:"Something went wrong!",status:false});
     }
 }
@@ -28,6 +38,7 @@ const getPendingInvites = async(req,res)=>{
         const pendingUsers = await userModel.find({isAdmin:"pending"}).skip((pageNum-1)*pageNum).limit(pageSize);
         res.status(200).json({message:"Successfull retreival",status:true,pending_invites:pendingUsers,total_pages:totalPages});
     } catch (error) {
+        console.log(error," at get pending invites")
         res.status(500).json({message:"Something went wrong!",status:false});
     }
 }
--- models/userModel.js
+++ models/userModel.js
@@ -73,6 +73,20 @@ const userSchema = mongoose.Schema({
     interestsHistory : [{
         type : String,
         required: false
+    }],
+    notifications : [{
+        from : {
+            type : String,
+            required : true
+        },
+        to : {
+            type : String,
+            required : true
+        },
+        message : {
+            type : String,
+            required : true
+        }
     }]
 });
 
